f5622481e9223ec2b82a0b73361aefdf48b23fdb
pavankalyananna <apk.pavan.07@gmail.com>
2025-12-07 20:45:35 +0530

    Delete src directory

--- src/main/java/com/example/ewallet/entity/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package com.example.ewallet.entity;
-
-public enum TransactionType {
-    RECHARGE,
-    TRANSFER_DEBIT,
-    TRANSFER_CREDIT
-}
-
--- src/main/java/com/example/ewallet/repository/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ewallet.repository;
-
-import com.example.ewallet.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Long> {
-}
